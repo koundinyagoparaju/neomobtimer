0489a65d75c458ed4c0c9686f0c28b38bd124654
Koundinya Goparaju <koundinya.goparaju@gmail.com>
2021-11-21 19:21:49 +0100

    Change title and description

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,8 @@ class CustomDocument extends Document {
             <Html lang="en">
                 <Head>
                     {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
-                    <title>Mob timer</title>
-                    <meta name="description" content="A timer which can be used in mob programming"/>
+                    <title>Mobtimer</title>
+                    <meta name="description" content="A timer which can be used in mob programming. Add names and start the timer. Get a reminder/notification to pass the keyboard when the timer is up. Share the timer with in team with shareable link"/>
                     <meta name="google-site-verification" content="LEX7F0JLWRkAZuamUP0vx_d3enp7tIlNBBhRLqoZBQw" />
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
